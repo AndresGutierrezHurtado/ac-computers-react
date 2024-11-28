1d8cd6874cfbd7828dab058becc78cd16198710f
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-11-28 17:33:15 -0500

    feat: add fetch handler

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: process.env.VITE_APP_URL,
         credentials: true,
     })
 );
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import React from "react";
+import { useGetData, usePostData } from "./hooks/useFetchApi.js";
+
 function App() {
+    const { data: products, loading: loadingProducts } = useGetData("/products");
+    console.log(products);
+
+    // usePostData("/user/login", {
+    //     user_email: "andres52885241@gmail.com",
+    //     user_password: "1234",
+    // });
+
+    console.log(products);
+    if (loadingProducts) return <h2> Cargando </h2>;
     return <></>;
 }
 
