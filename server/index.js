26ac3611aa6f835fac0d96f02bee5d72db0d7ab8
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-11-28 12:42:29 -0500

    feat: add express-session

--- server/index.js
+++ server/index.js
@@ -1,17 +1,35 @@
 import express from "express";
 import cors from "cors";
+import session from "express-session";
+import sequelizeStore from "connect-session-sequelize";
+import { sequelize } from "./config/database.js";
+const SequelizeStore = new sequelizeStore(session.Store);
+
+// routes
 import userRoutes from "./routes/user.routes.js";
 import productRoutes from "./routes/product.routes.js";
 
 const app = express();
 
+app.use(express.json());
 app.use(
     cors({
         origin: "http://localhost:3000",
         credentials: true,
     })
 );
-app.use(express.json());
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        store: new SequelizeStore({
+            db: sequelize,
+            table: "sessions",
+        }),
+        resave: false,
+        proxy: true,
+        saveUninitialized: false,
+    })
+);
 
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", productRoutes);
