2140a7c85944b000932d8735cd4233e95bd36413
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-11-28 15:22:56 -0500

    chore: add new records in the database

--- server/ac-computers.sql
+++ server/ac-computers.sql
@@ -63,7 +63,7 @@ CREATE TABLE `categories` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `categories` (`category_id`, `category_name`) VALUES
-(1, "producto"),
+(1, "computador"),
 (2, "componente");
 
 -- ---------------------------------------------------------------
@@ -78,6 +78,12 @@ CREATE TABLE `products` (
     `category_id` INT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+INSERT INTO `products` (`product_id`, `product_name`, `product_description`, `product_image_url`, `product_price`, `category_id`) VALUES
+('0928d6da-7443-43e5-bc66-ce7ddb7cbe43', 'PC Streamer Edition', 'PC diseñada para streaming y multitarea', '/images/products/default.jpg', 4800000, 1),
+('10cb4e1b-03bb-4e73-b86f-d5422be1f5ee', 'PC Warzone', 'PC para desarrollo y videojuegos', '/images/products/default.jpg', 5500000, 1),
+('32769f78-b0bc-49eb-b507-8c081437eef1', 'PC Creator Pro', 'PC optimizada para edición de video y diseño gráfico', '/images/products/default.jpg', 6200000, 1),
+('fc7adfe3-d907-4bd6-a8fd-c711261cf4e8', 'PC Budget Gaming', 'PC económica para juegos en 1080p', '/images/products/default.jpg', 3200000, 1);
+
 -- ---------------------------------------------------------------
 --
 -- Tabla de Detalles de especificaciones de los productos
@@ -87,6 +93,36 @@ CREATE TABLE `specs` (
     `spec_value` VARCHAR(50) NOT NULL,
     `product_id` VARCHAR(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `specs` (`spec_id`, `spec_key`, `spec_value`, `product_id`) VALUES
+('14497a7b-2de4-454d-8da1-cb5969c971ce', 'Procesador', 'Ryzen 7 7700X', '10cb4e1b-03bb-4e73-b86f-d5422be1f5ee'),
+('2320085c-6f89-47d0-8c59-b0c7f3be515f', 'Placa Madre', 'X570 TOMAHAWK', '0928d6da-7443-43e5-bc66-ce7ddb7cbe43'),
+('2ba890a0-474d-4568-8d32-3cb4dc2b8775', 'RAM', '32G DDR4 3600MHZ', '0928d6da-7443-43e5-bc66-ce7ddb7cbe43'),
+('2c78dc7c-d03e-4f84-a295-f1c556d55dea', 'Procesador', 'Intel Core i9-12900K', '32769f78-b0bc-49eb-b507-8c081437eef1'),
+('2e580c95-ecaf-46df-9f75-075d9baa4248', 'Procesador', 'Intel Core i5-12600K', 'fc7adfe3-d907-4bd6-a8fd-c711261cf4e8'),
+('32beb429-b33b-4a4b-b4cb-009a2ad67f92', 'Tarjeta Gráfica', 'RTX 4070 Ti 12GB', '0928d6da-7443-43e5-bc66-ce7ddb7cbe43'),
+('33bff5fd-d596-4d72-b95a-8089fdd690a8', 'Fuente de Poder', '850W 80+ GOLD', '32769f78-b0bc-49eb-b507-8c081437eef1'),
+('34142439-771a-4175-b791-725aefca6b8a', 'Chasis', 'Deepcool Matrexx 55', 'fc7adfe3-d907-4bd6-a8fd-c711261cf4e8'),
+('442c982c-33ad-454b-b515-3021864a7dc0', 'Chasis', 'Fan Y7 RGB', '10cb4e1b-03bb-4e73-b86f-d5422be1f5ee'),
+('452a8433-dcda-438f-8073-e68f301bcbe8', 'Placa Madre', 'Z690 AORUS PRO', '32769f78-b0bc-49eb-b507-8c081437eef1'),
+('4daf6f96-cf8d-4659-a046-1f9a23cfbb66', 'Chasis', 'Corsair iCUE 4000X', '0928d6da-7443-43e5-bc66-ce7ddb7cbe43'),
+('7249c1e9-4560-42de-80a8-a44fa484c0c9', 'Chasis', 'NZXT H510', '32769f78-b0bc-49eb-b507-8c081437eef1'),
+('9ee9f94d-3d46-4ed7-b20f-5190b75bf52d', 'Fuente de Poder', '750W 80+ GOLD', '0928d6da-7443-43e5-bc66-ce7ddb7cbe43'),
+('a4a9bc6c-5eee-421b-aff9-523f4b313955', 'Placa Madre', 'B560M', 'fc7adfe3-d907-4bd6-a8fd-c711261cf4e8'),
+('a8a838be-64aa-4249-895a-68de9b6b9767', 'Tarjeta Gráfica', 'RTX 4080 16GB', '32769f78-b0bc-49eb-b507-8c081437eef1'),
+('a8ba3394-d203-4668-8914-dfc357b01381', 'Almacenamiento', 'SSD 2TB M.2', '10cb4e1b-03bb-4e73-b86f-d5422be1f5ee'),
+('ae7dcefe-3676-430d-8c10-1925d90ca5f8', 'Fuente de Poder', '650W 80+ BRONCE', '10cb4e1b-03bb-4e73-b86f-d5422be1f5ee'),
+('b4a22ab2-a56b-47a7-a47a-52a6abb6926a', 'Almacenamiento', 'SSD 1TB M.2', 'fc7adfe3-d907-4bd6-a8fd-c711261cf4e8'),
+('b921e7dc-c858-4aa0-9bf0-ec12b0d1a119', 'RAM', '64G DDR5 6000MHZ', '32769f78-b0bc-49eb-b507-8c081437eef1'),
+('bae6b380-61e3-4841-a12d-f93692ea1f39', 'Tarjeta Gráfica', 'RTX 3060 12GB', 'fc7adfe3-d907-4bd6-a8fd-c711261cf4e8'),
+('c28ecaae-d368-4441-b999-feae35fa3775', 'Placa Madre', 'B650M', '10cb4e1b-03bb-4e73-b86f-d5422be1f5ee'),
+('c4019753-603a-4ce6-888b-0d1f406769ca', 'Almacenamiento', 'SSD 1TB NVMe + HDD 4TB', '32769f78-b0bc-49eb-b507-8c081437eef1'),
+('c4e3762e-dd86-437f-bf04-1157cc22d954', 'RAM', '64G DDR5 5600MHZ', '10cb4e1b-03bb-4e73-b86f-d5422be1f5ee'),
+('d9a68320-308f-4fb6-8a88-a104bf045da1', 'Tarjeta Gráfica', 'RTX 4060 8GB', '10cb4e1b-03bb-4e73-b86f-d5422be1f5ee'),
+('e2c44632-3e27-4059-89ce-950ce28e756c', 'RAM', '16G DDR4 3200MHZ', 'fc7adfe3-d907-4bd6-a8fd-c711261cf4e8'),
+('e410697f-6426-465f-917b-e2c62f8991ae', 'Fuente de Poder', '600W 80+ BRONCE', 'fc7adfe3-d907-4bd6-a8fd-c711261cf4e8'),
+('ef0738be-f245-40d7-a9da-7d33b799951f', 'Almacenamiento', 'SSD 1TB M.2 + SSD 2TB SATA', '0928d6da-7443-43e5-bc66-ce7ddb7cbe43'),
+('f69ed880-e444-4936-8477-3285d92dec2f', 'Procesador', 'Ryzen 9 5950X', '0928d6da-7443-43e5-bc66-ce7ddb7cbe43');
 
 -- ---------------------------------------------------------------
 --
