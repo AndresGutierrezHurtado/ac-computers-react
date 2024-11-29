326779c2692ad3356cdcab0193bd0d2826e5b6fd
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-11-29 01:29:19 -0500

    feat: add tailwind and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { useGetData, usePostData } from "./hooks/useFetchApi.js";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
-    const { data: products, loading: loadingProducts } = useGetData("/products");
-    console.log(products);
-
-    // usePostData("/user/login", {
-    //     user_email: "andres52885241@gmail.com",
-    //     user_password: "1234",
-    // });
-
-    console.log(products);
-    if (loadingProducts) return <h2> Cargando </h2>;
-    return <></>;
+    // const { data: computers, loading: loadingComputers } = useGetData("/products?category_id=1");
+    // const { data: components, loading: loadingComponents } = useGetData("/products?category_id=2");
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<h1> Home </h1>} />
+                <Route path="/computers" element={<h1> Computadoras </h1>} />
+                <Route path="/components" element={<h1> Componentes </h1>} />
+                <Route path="/login" element={<h1> Componentes </h1>} />
+                <Route path="/register" element={<h1> Componentes </h1>} />
+                <Route path="/admin/users" element={<h1> Componentes </h1>} />
+                <Route path="/admin/products" element={<h1> Componentes </h1>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
