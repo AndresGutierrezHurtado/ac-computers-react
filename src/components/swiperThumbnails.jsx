ee74cf69066120bf0d7ad9a4c07e1c6c628bb4be
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-12-01 15:12:31 -0500

    feat: add computer page

---
+++ src/components/swiperThumbnails.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+export default function SwiperThumbnails({ images = [], size = 300 }) {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+    const [current, setCurrent] = useState(0);
+
+    return (
+        <figure className="flex gap-5 w-full" style={{ width: `${size}px`, height: `${size - 120}px` }}>
+            <div className="w-fit relative">
+                <div className="badge badge-sm absolute top-2 right-2 z-10">
+                    {current + 1 || 1} / {images.length}
+                </div>
+
+                <Swiper
+                    style={{
+                        "--swiper-navigation-color": "#000",
+                        "--swiper-pagination-color": "#000",
+                        width: `${size - 120}px`,
+                    }}
+                    spaceBetween={10}
+                    navigation={true}
+                    thumbs={{ swiper: thumbsSwiper }}
+                    modules={[FreeMode, Navigation, Thumbs]}
+                    onSlideChange={(swiper) => setCurrent(swiper.activeIndex)}
+                    className="mySwiper2"
+                >
+                    {images.map((image) => (
+                        <SwiperSlide key={image.id}>
+                            <img src={image.url} alt={image.alt} />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+            <div className="w-[100px] h-full">
+                <Swiper
+                    onSwiper={setThumbsSwiper}
+                    spaceBetween={10}
+                    slidesPerView={3}
+                    watchSlidesProgress={true}
+                    modules={[FreeMode, Navigation, Thumbs]}
+                    className="mySwiper w-full h-full"
+                    style={{ height: `${size - 120}px` }}
+                    direction="vertical"
+                >
+                    {images.map((image) => (
+                        <SwiperSlide
+                            key={image.id}
+                            className="aspect-square w-[100%_!important]"
+                        >
+                            <img src={image.url} alt={image.alt} />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </figure>
+    );
+}
--- src/pages/computer.jsx
+++ src/pages/computer.jsx
@@ -1,16 +1,126 @@
 import React from "react";
 import { useParams } from "react-router";
+
+// Hooks
 import { useGetData } from "../hooks/useFetchApi.js";
+import SwiperThumbnails from "../components/swiperThumbnails.jsx";
 
 export default function Computer() {
-    const { data: computer, loading: loadingComputer } = useGetData(`/products/${useParams().id}`);
+    const { data: computer, loading: loadingComputer } = useGetData(
+        `/products/${useParams().id}`
+    );
+
+    const images = [
+        {
+            id: 1,
+            url: computer?.product_image_url,
+            alt: computer?.product_name,
+        },
+        {
+            id: 2,
+            url: "https://swiperjs.com/demos/images/nature-2.jpg",
+            alt: "nature-2",
+        },
+        {
+            id: 3,
+            url: "https://swiperjs.com/demos/images/nature-3.jpg",
+            alt: "nature-3",
+        },
+        {
+            id: 4,
+            url: "https://swiperjs.com/demos/images/nature-4.jpg",
+            alt: "nature-4",
+        },
+        {
+            id: 5,
+            url: "https://swiperjs.com/demos/images/nature-5.jpg",
+            alt: "nature-5",
+        },
+        {
+            id: 6,
+            url: "https://swiperjs.com/demos/images/nature-6.jpg",
+            alt: "nature-6",
+        },
+    ];
 
     if (loadingComputer) return <h1> Cargando producto </h1>;
     return (
-        <section className="w-full px-3">
-            <div className="w-full max-w-[12000px] mx-auto">
-                <div></div>
+        <>
+            <section className="w-full px-3">
+                <div className="w-full max-w-[1200px] mx-auto py-10">
+                    <div className="flex gap-10">
+                        <div className="w-fit">
+                            <SwiperThumbnails images={images} size={450} />
+                        </div>
+                        <div className="grow flex flex-col gap-2">
+                            <h1 className="text-5xl font-extrabold tracking-tight">
+                                {computer.product_name}
+                            </h1>
+                            <p className="text-lg grow">
+                                {computer.product_description}
+                            </p>
+                            <div className="text-3xl font-bold text-primary">
+                                {computer.product_discount > 0 && (
+                                    <p className="line-through text-2xl">
+                                        COP{" "}
+                                        {parseInt(
+                                            computer.product_price
+                                        ).toLocaleString("es-CO")}
+                                    </p>
+                                )}
+                                <p>
+                                    COP{" "}
+                                    {parseInt(
+                                        computer.product_price *
+                                            (1 -
+                                                computer.product_discount / 100)
+                                    ).toLocaleString("es-CO")}
+                                    {computer.product_discount > 0 &&
+                                        ` - ${computer.product_discount}%`}
+                                </p>
+                            </div>
+                            <button className="btn btn-primary">Volver</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className="w-full">
+                <div className="w-full max-w-[1200px] mx-auto">
+                    <img
+                        src="https://cdn.shopify.com/s/files/1/0482/9983/5557/files/PARA_PAGINA_WEB_BANNER_diciembre-min.jpg?v=1730930575"
+                        alt={computer.product_name}
+                    />
+                </div>
             </div>
-        </section>
+            <section className="w-full px-3">
+                <div className="w-full max-w-[1200px] mx-auto py-10">
+                    <div className="space-y-2">
+                        <h2 className="text-5xl font-extrabold tracking-tight mb-4">
+                            {computer.product_name}{" - "}
+                            <span className="text-3xl text-primary">
+                                {"COP "}
+                                {parseInt(
+                                    computer.product_price *
+                                        (1 - computer.product_discount / 100)
+                                ).toLocaleString("es-CO")}
+                            </span>
+                        </h2>
+                        <h2 className="text-2xl font-extrabold tracking-tight">
+                            Especificaciones del producto:
+                        </h2>
+                        <ol>
+                            {computer.specs.map((spec) => (
+                                <div key={spec.spec_id} className="flex gap-2">
+                                    <h3 className="text-lg font-bold">
+                                        {spec.spec_key}:
+                                    </h3>
+                                    <p>{spec.spec_value}</p>
+                                </div>
+                            ))}
+                        </ol>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 }
