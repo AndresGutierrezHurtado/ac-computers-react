fa51e99f2843d7225e7a632e65a753305b41f753
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-03-11 19:45:12 -0500

    chore: remove react proyect

--- src/hooks/useBase64.js
+++
@@ -1,5 +0,0 @@
-export const useBase64 = async (file) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    return new Promise((resolve) => reader.onload = () => resolve(reader.result));
-};
