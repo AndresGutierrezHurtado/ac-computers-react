d9a8641aa83ea7e2dd1a17fd4acaef9d4520271f
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-03-14 01:34:28 -0500

    build: add icons

---
+++ src/components/icons.jsx
@@ -0,0 +1,58 @@
+import {
+    FaStar as StarIcon,
+    FaGoogle as GoogleIcon,
+    FaFacebook as FacebookIcon,
+    FaWhatsapp as WhatsappIcon,
+    FaInstagram as InstagramIcon,
+    FaAt as AtIcon,
+    FaEye as EyeIcon,
+    FaEyeSlash as EyeSlashIcon,
+    FaPhoneAlt as PhoneIcon,
+    FaRegTrashAlt as TrashIcon,
+    FaBoxes as ProductsIcon,
+    FaUsers as UsersIcon,
+} from "react-icons/fa";
+import {
+    MdFileUpload as UploadIcon,
+    MdModeEditOutline as EditIcon,
+    MdFileDownload as DownloadIcon,
+} from "react-icons/md";
+import {
+    IoSearch as SearchIcon,
+    IoLocationOutline as LocationIcon,
+    IoHomeSharp as HomeIcon,
+} from "react-icons/io5";
+import {
+    IoIosArrowBack as BackIcon,
+    IoMdPricetags as PriceTagsIcon,
+    IoMdClose as CloseIcon,
+} from "react-icons/io";
+import { FaGear as GearIcon, FaComputer as ComputerIcon } from "react-icons/fa6";
+import { RxComponent1 as ComponentIcon } from "react-icons/rx";
+
+export {
+    StarIcon,
+    GoogleIcon,
+    FacebookIcon,
+    WhatsappIcon,
+    InstagramIcon,
+    AtIcon,
+    EyeIcon,
+    EyeSlashIcon,
+    PhoneIcon,
+    TrashIcon,
+    ProductsIcon,
+    UsersIcon,
+    GearIcon,
+    ComputerIcon,
+    UploadIcon,
+    EditIcon,
+    DownloadIcon,
+    SearchIcon,
+    LocationIcon,
+    HomeIcon,
+    BackIcon,
+    PriceTagsIcon,
+    CloseIcon,
+    ComponentIcon,
+};
