2a8a5b82764936a6430b585001354cf95338db3d
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-03-13 22:55:04 -0500

    feat: add products route

---
+++ src/app/api/products/route.js
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+
+import { Product, Category } from "@/database/models";
+
+export async function GET(resquest) {
+    const { searchParams } = new URL(resquest.url);
+
+    const page = parseInt(searchParams.get("page")) || 1;
+    const limit = parseInt(searchParams.get("limit")) || 10;
+    const offset = (page - 1) * limit;
+
+    const { rows: products, count } = await Product.findAndCountAll({
+        limit,
+        offset,
+        include: [
+            {
+                model: Category,
+                as: "category",
+            },
+        ],
+    });
+
+    return NextResponse.json({
+        success: true,
+        message: "Productos obtenidos correctamente",
+        count,
+        limit,
+        page,
+        data: products,
+    });
+}
+
+export async function POST(request) {
+    const { product: productJSON } = await request.json();
+
+    const product = await Product.create(productJSON);
+
+    return NextResponse.json({
+        success: true,
+        message: "Producto creado correctamente",
+        data: product,
+    });
+}
