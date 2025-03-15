004cae7bc0f843b4f69edccac9b7992076f8b83a
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-03-15 03:29:19 -0500

    feat: add fans animation

--- src/components/model.jsx
+++ src/components/model.jsx
@@ -1,16 +1,42 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { Suspense, useEffect, useRef } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Environment, OrbitControls, PerspectiveCamera, useGLTF } from "@react-three/drei";
 import gsap from "gsap";
 
 gsap.registerPlugin();
 
+useGLTF.preload("/scene.glb");
+
 const Modelo = () => {
     const modelRef = useRef();
     const { scene } = useGLTF("/scene.glb");
 
-    return <primitive ref={modelRef} object={scene} position={[0, 0, 0]} scale={1} />;
+    useFrame(() => {
+        if (modelRef.current) {
+            const fan1 = modelRef.current.getObjectByName("FAN1");
+            const fan2 = modelRef.current.getObjectByName("FAN2");
+            const fan3 = modelRef.current.getObjectByName("FAN3");
+            const fan4 = modelRef.current.getObjectByName("FAN4");
+
+            const hfan1 = modelRef.current.getObjectByName("HFAN1");
+            const hfan2 = modelRef.current.getObjectByName("HFAN2");
+
+            if (fan1 && fan2 && fan3 && fan4 && hfan1 && hfan2) {
+                fan1.rotation.y += 0.05;
+                fan2.rotation.y += 0.05;
+                fan3.rotation.y += 0.05;
+                fan4.rotation.y -= 0.05;
+                hfan1.rotation.y += 0.05;
+                hfan2.rotation.y += 0.05;
+            }
+        }
+    });
+
+    const { viewport } = useThree();
+    const scale = Math.min(viewport.width, viewport.height) / 8.5;
+
+    return <primitive ref={modelRef} object={scene} position={[0, 0, 0]} scale={scale} />;
 };
 
 const CameraController = ({ progress }) => {
@@ -18,8 +44,8 @@ const CameraController = ({ progress }) => {
 
     useEffect(() => {
         const positions = [
-            [-6, .8, 8],
-            [-9, 1, 0],
+            [-5.5, 0.8, 6.9],
+            [-10, 1, 0],
             [0, 10, 1],
             [0, 2, 9],
         ];
@@ -28,7 +54,7 @@ const CameraController = ({ progress }) => {
             gsap.to(camera.position, {
                 duration: 0.1,
                 x: 0,
-                y: 2,
+                y: 1,
                 z: 9,
             });
             return;
@@ -51,7 +77,6 @@ const CameraController = ({ progress }) => {
                     segmentProgress,
         ];
 
-        console.log(segment[2]);
         if (segment) {
             gsap.to(camera.position, {
                 duration: 0.1,
@@ -69,27 +94,27 @@ const Modelo3D = ({ progress }) => {
     return (
         <Canvas
             camera={{
-                position: [-5, 0, 9],
+                position: [-5.5, 0.8, 6.9],
                 fov: 50,
             }}
             style={{
                 width: "100%",
                 height: "100%",
+                pointerEvents: "none",
             }}
         >
-            <ambientLight intensity={10} position={[0, 0, 0]} />
-            <ambientLight intensity={20} position={[5, 0, 0]} />
+            {/* Lights and Environment */}
+            <Environment preset="city" environmentIntensity={0.04} />
+            <ambientLight intensity={20} position={[0, 0, 0]} />
             <ambientLight intensity={20} position={[-5, 0, 0]} />
             <directionalLight position={[5, 5, 5]} intensity={15} />
             <directionalLight position={[0, 5, 0]} intensity={10} />
-            <directionalLight position={[5, 0, 0]} intensity={10} />
-            <directionalLight position={[0, 0, 5]} intensity={10} />
-            <pointLight position={[3, 3, 3]} intensity={50} />
             <pointLight position={[-3, 3, 3]} intensity={50} />
-            <pointLight position={[3, 3, -3]} intensity={50} />
-            <pointLight position={[-3, 3, -3]} intensity={50} />
+            <pointLight position={[0, 0, 5]} intensity={100} />
+
+            {/* Model */}
             <Modelo />
-            <Environment preset="city" environmentIntensity={0.05} />
+
             <CameraController progress={progress} />
             <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
         </Canvas>
