28b5777b0a5209b1e06176bc7cb292255cc294eb
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-03-17 15:48:34 -0500

    feat: add new views

---
+++ src/app/admin/products/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Page() {
+    return (
+        <>
+            <section className="w-full px-3">
+                <div className="w-full max-w-[1200px] mx-auto mt-[100px]">
+                    <h1 className="text-5xl font-bold">Usuarios</h1>
+                </div>
+            </section>
+        </>
+    );
+}
---
+++ src/app/admin/users/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default async function Page() {
+    const response = await fetch(process.env.APP_DOMAIN + "/api/users");
+    const { data: users } = await response.json();
+
+    return (
+        <>
+            <section className="w-full px-3">
+                <div className="w-full max-w-[1200px] mx-auto mt-[100px]">
+                    <h1 className="text-5xl font-bold">Usuarios</h1>
+                </div>
+            </section>
+        </>
+    );
+}
